764a04962e4350748be010a6f8776aaf1e56261d
Vladimir G. Galkin <v.g.galkin@gmail.com>
2022-02-18 00:00:18 +0300

    Changes blank.yml

--- rfam.sql
+++ rfam.sql
@@ -1,1 +1,7 @@
-select * from full_region limit 1;
+#select * from full_region limit 1;
+SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
+FROM full_region fr, rfamseq rf, taxonomy tx
+WHERE rf.ncbi_id = tx.ncbi_id
+AND fr.rfamseq_acc = rf.rfamseq_acc
+AND tx.ncbi_id = 10116
+AND is_significant = 1;
